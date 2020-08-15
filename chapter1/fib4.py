c0302381a166756906f5c7c6c0eed65572b1bcdb
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-08-16 00:33:15 +0900

    learned @lru_cache decorator from functools

---
+++ chapter1/fib4.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fib4(n:int) -> int:
+    """
+    this decorator @lru_cache does caches all return values every time fib4() is called with new n value
+    :param n:
+    :return:
+    """
+
+    if n < 2:
+        return fib4(n)
+    return fib4(n-1) + fib4(n-2)
+
+if __name__ == "__main__":
+    print(fib4(50))
