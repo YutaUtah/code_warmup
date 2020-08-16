7f3696c3c26bbf532c166a54b3c29d2947058778
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-08-16 22:22:50 +0900

    until linear search p29

--- chapter2/dna_search,py.py
+++ chapter2/dna_search,py.py
@@ -5,24 +5,25 @@ Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]  # type alias for codons
 Gene = List[Codon]  # type alias for genes
 
-gene_str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+gene_str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT®"
 
-def string_to_gene(s: str):
+def string_to_gene(string):
     gene = []
-    for i in range(0, len(s), 3):
-        if (i + 2) >= len(s):  # don't run off end!
+    for i in range(0, len(string), 3):
+        if (i + 2) > len(string):
             return gene
+
         #  initialize codon out of three nucleotides
-        codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        codon = (Nucleotide[string[i]], Nucleotide[string[i + 1]], Nucleotide[string[i + 2]])
         gene.append(codon)  # add codon to gene
     return gene
 
 
 my_gene = string_to_gene(gene_str)
+print(my_gene)
 
-
-def linear_contains(gene, key_codon) -> bool:
-    for codon in gene:
+def linear_contains(my_gene, key_codon):
+    for codon in my_gene:
         if codon == key_codon:
             return True
     return False
@@ -33,5 +34,24 @@ gat = (Nucleotide.G, Nucleotide.A, Nucleotide.T) # one of codon example
 print(linear_contains(my_gene, acg))  # True
 print(linear_contains(my_gene, gat))  # False
 
-# this is ordinary linear search where you need to look at each index
-# ----------------------------------------------------------------------
+
+print(acg in my_gene)
+print(my_gene.__contains__(acg))
+# # this is ordinary linear search where you need to look at each index
+# # ----------------------------------------------------------------------
+
+def binary_contains(gene, key_codon):
+    low = 0
+    high = len(gene) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+
+    return False
+
+
