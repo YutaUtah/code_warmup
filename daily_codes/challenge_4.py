092b71b44d558b6a149e475900de89b11a4299ad
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-09-06 16:58:42 +0900

    challenge_5.py completed

--- daily_codes/challenge_4.py
+++ daily_codes/challenge_4.py
@@ -3,7 +3,9 @@ Problem:
 
 We are given two strings, A and B.
 
-A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example,
+if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B
+after some number of shifts on A.
 
 
 
@@ -31,3 +33,21 @@ SOLUTION: https://repl.it/@morningalgo/RotateString
 
 
 """
+A = 'abcde'
+B = 'abced'
+
+
+def rotateString(A, B):
+    if len(A) != len(B):
+        return False
+    if len(A) == 0:
+        return True
+
+    for s in range(len(A)):
+        for i in range(len(A)):
+            if all(A[(s + i) % len(A)] == B[i]):
+                return True
+    return False
+
+a =rotateString(A,B)
+print(a)
